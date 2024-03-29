9008c9014721e9763b8c49b48281fc135d91f728
Filip Leo Bernhard Frisk <filip.leo.bernhard.frisk@cern.ch>
2024-03-29 14:13:03 +0100

    improved histograms

--- main.py
+++ main.py
@@ -17,9 +17,11 @@ SIGNAL_CHANNEL = ['VBF']
 BACKGROUND_CHANNEL = ['WW', 'Zjets', 'ttbar'] # order in size event weight or MC samples
 
 SELECTED_OTHER_VARIABLES = ['eventType','label','eventNumber','weight']
-SELECTED_PHYSICAL_VARIABLES = ['DPhijj', 'mll', 'mT', 'DYjj', 'mjj', 'ptTot', 'mL1J1', 'mL1J2', 'mL2J1', 'mL2J2','ptJ1','ptJ2','ptJ3','METSig']
-SELECTED_PHYSICAL_VARIABLES_UNITS = ['rad','GeV','GeV','length???','GeV','GeV','GeV','GeV','GeV','GeV','GeV','GeV','GeV','Unitless'] # eta_l_centrality missing?
-
+SELECTED_PHYSICAL_VARIABLES = ['DPhijj', 'mll', 'mT', 'DYjj', 'mjj', 'ptTot', 'mL1J1', 'mL1J2', 'mL2J1', 'mL2J2','ptJ1','ptJ2','ptJ3','METSig'] # eta_l_centrality missing?
+SELECTED_PHYSICAL_VARIABLES_UNITS = ['rad?','?eV','?eV','','?eV','?eV','?eV','?eV','?eV','?eV','?eV','?eV','?eV',''] # Is it really GeV? units? '' empty for unitless
+### DATA VISUALIZATION ###
+OVERFLOW_UNDERFLOW_PERCENTILE = {'lower_bound': 5, 'upper_bound': 95} # ex 1% and 99% percentile, all data outside this range will be added to the last and first bin respectively
+BINS = 20
 ### DATA PREPROCESSING ###
 TRAIN_DATA_SIZE = 0.8
 RANDOM_SEED = None # 42
@@ -34,9 +36,11 @@ with open(f'{DATA_RELATIVE_FOLDER_PATH+DATA_FILENAME_WITHOUT_FILETYPE}.pkl', 'rb
     df = pickle.load(f)
 
 # create kinetic plots for entire dataframe
+    
 for variable, unit in zip(SELECTED_PHYSICAL_VARIABLES, SELECTED_PHYSICAL_VARIABLES_UNITS):
-    plot_one_physical_variable(df, variable, unit, SIGNAL_CHANNEL , BACKGROUND_CHANNEL, CUT,DATA_FILENAME_WITHOUT_FILETYPE)
-    break 
+    plot_one_physical_variable(df, variable, unit, SIGNAL_CHANNEL , BACKGROUND_CHANNEL, CUT,DATA_FILENAME_WITHOUT_FILETYPE,OVERFLOW_UNDERFLOW_PERCENTILE,BINS)     
+
+
 # run if you want to pre-process the data
 #for K_FOLD in range(1,6):
 #    print(f"Starting analysing K-Fold: {K_FOLD}")
--- tools/create_pretty_histograms.py
+++ tools/create_pretty_histograms.py
@@ -5,7 +5,7 @@ import numpy as np
 import os 
 
 
-def plot_one_physical_variable(df, physical_variable, unit, signal, background, cut,DATA_FILENAME_WITHOUT_FILETYPE):
+def plot_one_physical_variable(df, physical_variable, unit, signal, background, cut,DATA_FILENAME_WITHOUT_FILETYPE, OVERFLOW_UNDERFLOW_PERCENTILE,BINS):
     plt.figure()
     
     plot_data = []
@@ -31,31 +31,46 @@ def plot_one_physical_variable(df, physical_variable, unit, signal, background, 
     plot_weights.append(df.loc[df['label'] == signal[0]]['weight'])
     plot_labels.append(signal[0])
     
+    
     # Calculate the 5th and 95th percentile to exclude extreme outliers
-    
     flattened_data = np.concatenate(plot_data)
-    lower_bound = np.percentile(flattened_data, 5)    
-    upper_bound = np.percentile(flattened_data, 95)
+    lower_bound = np.percentile(flattened_data, OVERFLOW_UNDERFLOW_PERCENTILE['lower_bound'])    
+    upper_bound = np.percentile(flattened_data, OVERFLOW_UNDERFLOW_PERCENTILE['upper_bound'])
 
-    # get data not included in bound from flattened data
-    data_outside_bounds = flattened_data[(flattened_data < lower_bound) | (flattened_data > upper_bound)] # 10% of data of course
-    print(len(data_outside_bounds)/len(flattened_data)*100, '% of data is outside the bounds')
+    bounds = (lower_bound, upper_bound) 
 
-    bounds = (lower_bound, upper_bound) # TODO implement overflow and underflow on last bin, add 50% of the data in last and first bin respectively?
+    Overflow = len(flattened_data[flattened_data > upper_bound])
+    Underflow = len(flattened_data[flattened_data < lower_bound])
+
+    Overflow_ratio = Overflow/len(flattened_data)
+    Underflow_ratio = Underflow/len(flattened_data)
     
-    SCALE = 5000 # TODO fixate dynamically later
+    print(f"With added bounds of [{OVERFLOW_UNDERFLOW_PERCENTILE['lower_bound']},{OVERFLOW_UNDERFLOW_PERCENTILE['upper_bound']}] percentile we get {Underflow} Underflow events or {Underflow_ratio*100:.2f}% and {Overflow} Overflow events or {Overflow_ratio*100:.2f}%" )
+
+    # Add overflow and underflow to the last and first bin respectively
+    for channel in plot_data:
+        channel = np.clip(channel, lower_bound, upper_bound)
+
+    SIGNAL_ENVELOPE_SCALE = 5000 # easier to guess than to scale dynamically 
 
     # get signal data and scale it to the largest background
     signal_scaled = df.loc[df['label'] == signal[0]][physical_variable] 
-    signal_weight = df.loc[df['label'] == signal[0]]['weight']*SCALE
-    signal_label = f"{signal[0]} x {SCALE}"
+    signal_weight = df.loc[df['label'] == signal[0]]['weight']*SIGNAL_ENVELOPE_SCALE
+    signal_label = f"{signal[0]} x {SIGNAL_ENVELOPE_SCALE}"
+
+    # Normalize weights by the bin width
+    bin_width = (upper_bound - lower_bound)/BINS
+    print(f"Bin width of {bin_width:.2f}")
+    normalized_signal_weight = [w / bin_width for w in signal_weight]
+    normalized_plot_weights = [[w / bin_width for w in single_stack] for single_stack in plot_weights]
+
     
     # plot signal shape and stacked shape of background and signal
-    plt.hist(signal_scaled, bins=50, label=signal_label, weights=signal_weight, range=bounds, histtype='step', color=colors[-1])
-    plt.hist(plot_data, bins=50, label=plot_labels, weights=plot_weights, stacked=True, range=bounds, alpha=0.5, histtype='stepfilled', color=colors)
-
+    plt.hist(signal_scaled, bins=BINS, label=signal_label, weights=normalized_signal_weight, range=bounds, histtype='step', color=colors[-1])
+    plt.hist(plot_data, bins=BINS, label=plot_labels, weights=normalized_plot_weights, stacked=True, range=bounds, alpha=0.5, histtype='stepfilled', color=colors)
+    plt.xlim(lower_bound, upper_bound) # No white space 
     plt.xlabel(r'${} \ [{}]$'.format(physical_variable, unit))
-    plt.ylabel('Events')
+    plt.ylabel(f'Events/{bin_width:.2f} {unit}')
     plt.legend(loc='upper right')
     
     subtext_latex = r'Internal KTH: Run 2 Monte Carlo data, all campaigns' + '\n' + r'$HWW \rightarrow WW^* \rightarrow l\nu l\nu$' + '\n' + r'SF VBF $N_{jet}$ $\geq$ 2' + '\n' + r'rootfilename: {}'.format(cut)
@@ -63,14 +78,14 @@ def plot_one_physical_variable(df, physical_variable, unit, signal, background, 
     atlasify(subtext = subtext_latex , sub_font_size = 8)
     
     ax = plt.gca() # Get the current axis
-    ax.ticklabel_format(style='scientific', axis='both', scilimits=(0,0))
+    ax.ticklabel_format(style='plain', axis='both', scilimits=(0,0))
 
     os.chdir('plots/')
     plt.savefig(f'prefit_histogram_{DATA_FILENAME_WITHOUT_FILETYPE}_{physical_variable}.png')
     os.chdir('..')
     
     # print what you have saved in short format
-    print(f"Saved prefit_histogram_{DATA_FILENAME_WITHOUT_FILETYPE}_{physical_variable}.png in plots/ folder.")
+    print(f"Saved prefit_histogram_{DATA_FILENAME_WITHOUT_FILETYPE}_{physical_variable}.png in plots/ .")
     
     #plt.show()
     plt.close()
